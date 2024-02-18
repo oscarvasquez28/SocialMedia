26ef244acd7521be3bb7e8739c3cc5a18a4f8b82
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2024-02-17 18:10:00 -0600

    Javascript, Bootstrap y demás
    se agregó funcionalidades de javascript, se utilizó bootstrap para incluir íconos y se complementó la página

---
+++ script.js
@@ -0,0 +1,25 @@
+var settingsMenu = document.querySelector(".settings-menu")
+var Hide = document.querySelector(".advertisement")
+var SeeAll = document.querySelector(".event-all")
+var SeeAllInactive = document.querySelector(".sidebar-title-SeeAll")
+var CloseActive = document.querySelector(".sidebar-title-Close")
+var CloseAd = document.querySelector(".sidebar-title-closeAd")
+var ShowAd = document.querySelector(".sidebar-title-showAd")
+
+function settingsMenuToggle(){
+    settingsMenu.classList.toggle("settings-menu-height");
+}
+
+function AdvertisementHide(){
+    Hide.classList.toggle("advertisement-height");
+    CloseAd.classList.toggle("sidebar-title-closeAd-Inactive");
+    ShowAd.classList.toggle("sidebar-title-showAd-Active");
+}
+
+function EventAll(){
+    SeeAll.classList.toggle("event-all-height"); /*Esconde los demás eventos*/ 
+    SeeAllInactive.classList.toggle("sidebar-title-SeeAll-Active"); /*Esconde el See All*/
+    CloseActive.classList.toggle("sidebar-title-Close-Inactive"); /* Muestra el Close */
+}
+
+
